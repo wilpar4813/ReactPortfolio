06b34205ab77471ef310485272747a98ad5ba21e
Tripp Parham <57144903+wilpar4813@users.noreply.github.com>
2020-04-08 22:38:38 -0400

    Update App.js
    Change Browserrouter to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Resume from "./components/pages/Resume";
 import Footer from "./components/Footer";
