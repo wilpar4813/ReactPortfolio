f223ec6f0aa7d23f27173c31775ce3c9c7e01c8c
Tripp's Repo <tripp.parham@me.com>
2020-05-03 12:58:13 -0400

    update to resume

--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -3,18 +3,27 @@ import React from "react";
 function ResumeCard() {
     return (
         <div className="resume-container">
-            <h1>My Mission</h1>
+            <u>
+                <h1>Summary</h1>
+            </u>
 
             <center>
                 <p>
-                    My Mission is to launch a new career in full stack web
-                    development with a company that is looking for
-                    self-motivated innovator
+                    Seeking FULL STACK web developer role leveraging six years
+                    deep hands-on experience in datacenter systems engineering
+                    and working simultaneously with multiple software
+                    development teams. Recently certifying in Full Stack Web
+                    Development from the University of North Carolina, I
+                    acquired skills in JavaScript, Node, React, CSS, Bootstrap
+                    and responsive web design with an A+ average. Looking to
+                    bring a unique perspective and skillset to a passionate
+                    quality-driven team that works together to create a better
+                    customer experience and a successful profitable company.
                 </p>
             </center>
             <hr />
             <u>
-                <h1>Skills</h1>
+                <h1>Technical Skills</h1>
             </u>
             <h5>Full Stack Web Development Technologies</h5>
             <p>
@@ -22,24 +31,18 @@ function ResumeCard() {
                 Jquery, JS Timers, API's, AJAX, Local Storage, Node JS,
                 Constructors, Callbacks, MySQL, Node-Express Server,
                 Handlebars/Templating, Sequelize, Test Driven Development,
-                Agile, MongoDB, React JS, MS Visual Studio Code
+                Agile, MongoDB, React JS, MS Visual Studio Code, iCode
             </p>
-            <h5>Converged and Hyper-Converged Storage Array Technologies</h5>
-            <p>VxRack with ScaleIO, Vblock 100,200,300,500,and 700 Series</p>
-            <h5>Network Technologies</h5>
+            <h5>Network and SAN Technologies</h5>
             <p>
-                Cisco Nexus 1000v, Nexus 2000 FEX, Nexus 3000, Nexus 5000, Nexus
-                7000, Nexus 9000 (NX-OS/ACI), MDS 9148, MDS 9248, MDS 9710
-                Zoning, VLAN Management, Port Channels, Virtual Port Channels,
-                Trunks, HSRP, WWPN, Spanning Tree, Fabric Path, CDP, FC, FCoE,
-                iSCSI, NAS, NFS, CIFS
+                Cisco Nexus, Cisco MDS, SAN Zoning, VLAN Management, Port
+                Channels, Virtual Port Channels, Trunks, HSRP, WWPN, Spanning
+                Tree, FabricPath, CDP, FC, FCoE, iSCSI, NAS, NFS, CIFS
             </p>
-            <h5>Compute Technologies</h5>
+            <h5>Server Technologies</h5>
             <p>
-                Cisco UCS 6148, UCS 6248, UCS Manager, UCS B-Series, UCS
-                C-Series, Multi UCSM Domain, UCS Mini, Microsoft Windows Server
-                2008/2012, Active Directory, DNS, DHCP, IPv4, IPv6, Linux, Red
-                Hat, Apple OS X, Dell iDRAC
+                Cisco UCS, Microsoft Windows Server 2008/2012, Active Directory,
+                DNS, DHCP, IPv4, IPv6, Linux, Red Hat, Apple OS X, Dell iDRAC
             </p>
             <h5>Virtualization Technologies</h5>
             <p>
@@ -47,11 +50,8 @@ function ResumeCard() {
                 Client, vCenter Server Appliance, vMotion, Standard and
                 Distributed Switching, vCenter Update Manager, SSO
             </p>
-            <h5>Storage Technologies</h5>
-            <p>
-                EMC VMAX, VNX, Unity, VPLEX, Solutions Enabler (SMI-S), SYMCLI,
-                Unisphere, ScaleIO
-            </p>
+            <h5>EMC Storage Technologies</h5>
+            <p>VBLOCK, VMAX, VXRack, VNX, Unity, VPLEX, Unisphere, ScaleIO</p>
             <h5> Infrastructure Technologies</h5>
             <p>
                 Cat 6, Fiber, Panduit, SolarWinds CatTools, Cable Management,
@@ -59,9 +59,26 @@ function ResumeCard() {
             </p>
             <hr />
             <u>
-                <h1>Work Experience</h1>
+                <h1>Experience</h1>
             </u>
             <ul>
+                <li>
+                    <h3>Customer Support Engineer SAN MDS</h3>
+                    <h5>May 2017 – July 2017</h5>
+                    <h5>Cisco Systems via contract with Randstad</h5>
+                    <h5>Morrisville, NC</h5>
+                    <ul>
+                        <li>
+                            Provided MDS related Tier 2 and Tier 3 support
+                            working with customers on live customer networks via
+                            WebEx to resolving host to storage connectivity and
+                            zoning issues as well as instructive assistance to
+                            those that did not understand technologies like FC
+                            Aliases and Enhanced Zoning or with common issues
+                            like upgrades or licensing.
+                        </li>
+                    </ul>
+                </li>
                 <li>
                     <h3>Lab Systems Engineer</h3>
                     <h5>August 2014 – July 2016</h5>
@@ -72,7 +89,7 @@ function ResumeCard() {
                     <h5>Durham, NC</h5>
                     <ul>
                         <li>
-                            Provided senior level knowledge of the physical and
+                            Top ticket closer with knowledge of the physical and
                             logical build process of VCE Vblock Converged
                             Infrastructure (CI) platforms covering tasks
                             including the installation and maintenance of Cisco
@@ -80,32 +97,29 @@ function ResumeCard() {
                             ScaleIO, EMC VNX, Linux, and Window 2008/2012 Server
                         </li>
                         <li>
-                            Provided hands on installation, configuration, and
-                            management of over two hundred racks of VCE
-                            Converged and Hyper-converged cloud computing
-                            platforms, with additional duties including testing,
-                            documenting, maintaining, incident resolution, and
-                            working with vendor support Installed and maintained
-                            the following technology: Cisco UCS chassis, C and B
-                            series servers, Fabric Interconnects, Fabric
-                            Extenders (FEX), MDS SAN switches, Nexus/ACI, L2/L3
+                            Recognized with cash bonus for large physical
+                            migration of a large scale multi-domain R&D lab
+                            environment including the installation,
+                            configuration, and management of over two hundred
+                            racks of VCE Converged and Hyper-converged cloud
+                            computing platforms, with additional duties
+                            including testing, documenting, maintaining,
+                            incident resolution, and working with vendor support
+                        </li>
+                        <li>
+                            Learned how to install and maintain the following
+                            technology: Cisco UCS chassis, C and B series
+                            servers, Fabric Interconnects, Fabric Extenders
+                            (FEX), MDS SAN switches, Nexus/ACI, L2/L3
                             networking, EMC VNXe, VNX, and VMAX storage arrays,
                             VMware vCenter and ESXi 5.0-6.1
                         </li>
                         <li>
-                            Preformed Release Certification Matrix (RCM)
-                            upgrades, quarterly storage rotations, and advanced
-                            troubleshooting on VCE’s entire portfolio of
-                            products, including Vblock and VxRack systems owned
-                            by VCE’s Engineering and Development teams.{" "}
-                        </li>
-                        <li>
-                            Provided technical support to other VCE teams and
-                            mentored new staff on the installation,
-                            administration, and support of a large scale
-                            multi-domain R&D lab environment Please see addendum
-                            for more detailed information on Vblock related
-                            tasks completed
+                            The software development teams go-to person
+                            performing upgrades, quarterly storage rotations,
+                            and advanced troubleshooting on VCE’s entire
+                            portfolio of products, including Vblock and VxRack
+                            systems
                         </li>
                     </ul>
                 </li>
@@ -119,33 +133,12 @@ function ResumeCard() {
                     <h5>RTP, NC</h5>
                     <ul>
                         <li>
-                            Provided tier 1 support in a Cisco Network Operation
-                            Center (CNOC), which provided network support for
-                            Sprint and for the North Carolina State Employees
-                            Credit Union
-                        </li>
-                        <li>
-                            Responded to alerts logged by Cisco ARS 9000, 901,
-                            and 903 routers as well as with 2900 series routers
-                        </li>
-                        <li>
-                            Troubleshot complex enterprise networks to quickly
-                            resolve downtime and outages
-                        </li>
-                        <li>
-                            Responded to network management system alarms set by
-                            Cisco Prime according to established processes
-                        </li>
-                        <li>
-                            Resolved customer’s issues by setting expectations
-                            and meeting/exceeding those expectations to achieve
-                            the highest level of customer satisfaction Utilized
-                            the Cisco technical service lab to recreate and
-                            resolve customer issues.
-                        </li>
-                        <li>
-                            Effectively communicate issues to engineers within
-                            the support team
+                            Received positive reviews providing tier 1 network
+                            support in a Cisco Network Operation Center (CNOC)
+                            to Sprint and to the North Carolina State Employees
+                            Credit Union by setting expectations and exceeding
+                            those expectations to achieve the highest level of
+                            customer satisfaction
                         </li>
                     </ul>
                 </li>
@@ -157,17 +150,17 @@ function ResumeCard() {
                     <h5>Durham, NC</h5>
                     <ul>
                         <li>
-                            Developed software solutions based on detailed
-                            specifications from technology/systems consultants
-                            for the EMC Durham Center of Excellence Datacenter
-                            Cloud Core product
+                            Promoted to Software Engineer after volunteering and
+                            successfully building a website for the Durham
+                            Datacenter.
                         </li>
                         <li>
-                            Developed and/or executed code implementation
-                            according to Agile methodology  Provided technical
-                            assistance to the development of solutions to a
-                            limited variety of software engineering problems of
-                            limited scope and complexity
+                            As a self-taught Java Developer, developed software
+                            solutions based on detailed specifications and
+                            provided technical assistance to the development of
+                            solutions to a variety of software engineering
+                            problems of limited scope and complexity according
+                            to Agile methodology
                         </li>
                     </ul>
                 </li>
@@ -179,27 +172,22 @@ function ResumeCard() {
                     <h5>Durham, NC</h5>
                     <ul>
                         <li>
-                            Responded to ticketing request for general
-                            datacenter lab support which included labeling and
-                            installing cables to SAN switches, various band
-                            servers, and EMC storage arrays
+                            Learned datacenter infrastructure while assisting in
+                            the migration of over 14000 storage arrays cabinets
+                            to the new datacenter
                         </li>
                         <li>
-                            Dressed cables per Durham COE Datacenter labs
-                            standard, verified network connectivity, and
-                            documented all connections
+                            Closed 1800 of my team's 4000 tickets in my first
+                            year.
                         </li>
                         <li>
-                            Provided EMC Symmetrix VMAX engineering rotation
-                            support
-                        </li>
-                        <li>
-                            Provided support in the coordinate moves and
-                            rotations of equipment in EMC Durham Datacenter labs{" "}
-                        </li>
-                        <li>
-                            Provided lab support assistance including
-                            troubleshooting and swapping out system components
+                            Received Gold and Silver Cash Awards and hundreds of
+                            positive reviews for high successful ticket closure
+                            rate while responding to ticketing request for
+                            datacenter lab support which included labeling,
+                            installing, dressing, and documenting cables to SAN
+                            switches, various band servers, and EMC storage
+                            arrays
                         </li>
                     </ul>
                 </li>
