8250d6927098d5ec0c11ce8f80743cbaef6644e8
Tripp's Repo <tripp.parham@me.com>
2020-07-24 00:59:08 -0400

    Updates

--- precache-manifest.131432afd440b79fb4d80fd9682f9ea9.js
+++ precache-manifest.131432afd440b79fb4d80fd9682f9ea9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "54abe6b8b4dc437dc111cde7d944324f",
+    "revision": "c77cf0081050605a95a14f5b33ef7cae",
     "url": "/ReactPortfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "747275da5ad1c7361a36",
-    "url": "/ReactPortfolio/static/css/main.6e7a1751.chunk.css"
+    "revision": "6906f1fa5450a4fd8736",
+    "url": "/ReactPortfolio/static/css/main.074affab.chunk.css"
   },
   {
     "revision": "f198e5d07a987553d5ef",
@@ -20,12 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/js/2.7ce3a80b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "747275da5ad1c7361a36",
+    "revision": "6906f1fa5450a4fd8736",
     "url": "/ReactPortfolio/static/js/main.fffac29c.chunk.js"
   },
   {
     "revision": "3f21a18d73677ab16af3",
     "url": "/ReactPortfolio/static/js/runtime-main.6cb6df91.js"
+  },
+  {
+    "revision": "2541cc282e0dfa8d416ab91b1e34b8fa",
+    "url": "/ReactPortfolio/static/media/cork-board.2541cc28.png"
   },
   {
     "revision": "5036e6ddbba44331d6aedf0526807e90",
